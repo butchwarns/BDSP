328e2fb3126ad6c1227666f509ffaa86fb30813f
Butch Warns <contact@butchwarns.de>
2023-06-08 23:51:42 +0200

    Write decibels documentation

--- source/decibels.h
+++ source/decibels.h
@@ -5,16 +5,36 @@
 
 namespace decibels
 {
+    /**
+     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     *
+     * @param db_val
+     * @return double
+     */
     inline double db_to_raw_gain(const double db_val)
     {
         return pow(10.0, db_val / 20.0);
     }
 
+    /**
+     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     *
+     * @param db_val
+     * @return float
+     */
     inline float db_to_raw_gain(const float db_val)
     {
         return powf(10.0f, db_val / 20.0f);
     }
 
+    /**
+     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     *        If value lies below given threshold, this function returns 0
+     *
+     * @param db_val
+     * @param db_threshold
+     * @return double
+     */
     inline double db_to_raw_gain_off(const double db_val, const double db_threshold)
     {
         if (db_val <= db_threshold)
@@ -27,6 +47,14 @@ namespace decibels
         }
     }
 
+    /**
+     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     *        If value lies below given threshold, this function returns 0
+     *
+     * @param db_val
+     * @param db_threshold
+     * @return float
+     */
     inline float db_to_raw_gain_off(const float db_val, const double db_threshold)
     {
         if (db_val <= db_threshold)
@@ -39,11 +67,23 @@ namespace decibels
         }
     }
 
+    /**
+     * @brief Converts raw gain coefficient to decibel (relative to 1.0) value
+     *
+     * @param raw_gain
+     * @return double
+     */
     inline double raw_gain_to_db(const double raw_gain)
     {
         return 20.0 * log10(raw_gain);
     }
 
+    /**
+     * @brief Converts raw gain coefficient to decibel (relative to 1.0) value
+     *
+     * @param raw_gain
+     * @return float
+     */
     inline float raw_gain_to_db(const float raw_gain)
     {
         return 20.0f * log10f(raw_gain);
