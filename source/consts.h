cef6003438894f6a3fd24edbcd1f93659ecfe8b0
Butch Warns <contact@butchwarns.de>
2023-07-20 14:40:43 +0200

    Add filter frequency limit constants

--- source/consts.h
+++ source/consts.h
@@ -18,6 +18,16 @@ namespace bdsp
          */
         constexpr double TWO_PI = 2.0 * 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899;
 
+        /**
+         * @brief Minimum filter cutoff frequency, lower threshold of human hearing
+         */
+        constexpr double MIN_FILTER_FREQ = 20.0;
+
+        /**
+         * @brief Maximum filter cutoff frequency, upper threshold of human hearing
+         */
+        constexpr double MAX_FILTER_FREQ = 20480.0;
+
     } // namespace consts
 } // namespace bdsp
 
