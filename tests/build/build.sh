a53a7b316a7fbd4f93d8123ad6d9cf5ea74298bb
Butch Warns <contact@butchwarns.de>
2023-12-15 22:17:44 +0100

    Add prints to build script

--- tests/build/build.sh
+++ tests/build/build.sh
@@ -4,6 +4,8 @@
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
+echo "\nRunning cmake...\n"
 cmake .. -GUnix\ Makefiles -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
 
+echo "\nBuilding tests...\n"
 cmake --build .
