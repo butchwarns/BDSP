e142131669d3db4b81dd579d5c48c6572b6b8d4e
Butch Warns <contact@butchwarns.de>
2025-01-14 20:20:02 +0100

    Do renamings in decibel.h

--- source/bdsp/decibel.h
+++ source/bdsp/decibel.h
@@ -7,87 +7,87 @@ namespace bdsp::decibel
 {
 
     /**
-     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     * @brief Converts decibel (relative to 1.0) value to linear gain factor
      *
-     * @param db_val
+     * @param val_db
      * @return double
      */
-    inline double db_to_raw_gain(const double db_val)
+    inline double db_to_linear(const double val_db)
     {
-        return pow(10.0, db_val / 20.0);
+        return pow(10.0, val_db / 20.0);
     }
 
     /**
-     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     * @brief Converts decibel (relative to 1.0) value to linear gain factor
      *
-     * @param db_val
+     * @param val_db
      * @return float
      */
-    inline float db_to_raw_gain(const float db_val)
+    inline float db_to_linear(const float val_db)
     {
-        return powf(10.0f, db_val / 20.0f);
+        return powf(10.0f, val_db / 20.0f);
     }
 
     /**
-     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     * @brief Converts decibel (relative to 1.0) value to linear gain factor
      *        If value lies below given threshold, this function returns 0
      *
-     * @param db_val
-     * @param db_threshold
+     * @param val_db
+     * @param threshold_db
      * @return double
      */
-    inline double db_to_raw_gain_off(const double db_val, const double db_threshold)
+    inline double db_to_linear_off(const double val_db, const double threshold_db)
     {
-        if (db_val <= db_threshold)
+        if (val_db <= threshold_db)
         {
             return 0.0;
         }
         else
         {
-            return db_to_raw_gain(db_val);
+            return db_to_linear(val_db);
         }
     }
 
     /**
-     * @brief Converts decibel (relative to 1.0) value to raw gain coefficient
+     * @brief Converts decibel (relative to 1.0) value to linear gain factor
      *        If value lies below given threshold, this function returns 0
      *
-     * @param db_val
-     * @param db_threshold
+     * @param val_db
+     * @param threshold_db
      * @return float
      */
-    inline float db_to_raw_gain_off(const float db_val, const double db_threshold)
+    inline float db_to_linear_off(const float val_db, const double threshold_db)
     {
-        if (db_val <= db_threshold)
+        if (val_db <= threshold_db)
         {
             return 0.0f;
         }
         else
         {
-            return db_to_raw_gain(db_val);
+            return db_to_linear(val_db);
         }
     }
 
     /**
-     * @brief Converts raw gain coefficient to decibel (relative to 1.0) value
+     * @brief Converts linear gain factor to decibel (relative to 1.0) value
      *
-     * @param raw_gain
+     * @param gain_linear
      * @return double
      */
-    inline double raw_gain_to_db(const double raw_gain)
+    inline double linear_to_db(const double gain_linear)
     {
-        return 20.0 * log10(raw_gain);
+        return 20.0 * log10(gain_linear);
     }
 
     /**
-     * @brief Converts raw gain coefficient to decibel (relative to 1.0) value
+     * @brief Converts linear gain factor to decibel (relative to 1.0) value
      *
-     * @param raw_gain
+     * @param gain_linear
      * @return float
      */
-    inline float raw_gain_to_db(const float raw_gain)
+    inline float linear_to_db(const float gain_linear)
     {
-        return 20.0f * log10f(raw_gain);
+        return 20.0f * log10f(gain_linear);
     }
 
 } // namespace bdsp::decibel
